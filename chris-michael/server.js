3876ed4c5f64b4df336511f2ff6f2305d2cf8506
Adutchguy <mdm_8@hotmail.com>
2017-04-27 13:53:18 -0700

    added the /about route

--- chris-michael/server.js
+++ chris-michael/server.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
 
 app.get('/new', (request, response) => response.sendFile('new.html', {root: './public'}));
+app.get('/about', (request,response) => response.sendFile('index.html', {root: './public'}));
 app.get('/admin', (request, response) => response.sendFile('admin.html', {root: './public'}));
 app.get('/articles', (request, response) => {
   client.query(`
